0c92a81261711b523d809c4263b3936d19e66911
Ruben Jenster <r.jenster@drachenfels.de>
2013-07-30 18:36:46 +0200

    Use plugin class nested into the plugin module to create the plugin module instance. Expose loaded plugins through the metadata of the example group.

--- lib/rspec-plugins/core.rb
+++ lib/rspec-plugins/core.rb
@@ -1,6 +1,16 @@
 require 'rspec/core'
 require 'ostruct'
 
+=begin
+        * listener registration before(:all)
+        * example_group_started
+        * before(:all) methods added from helpers
+        * example_started
+        * after(:all) methods added from helpers
+        * example_group_finished
+        * listener de-registration after(:all)
+=end
+
 module RSpec::Plugins
   module Core
 
@@ -9,17 +19,15 @@ module RSpec::Plugins
       plugin_module.extend(ClassMethods)
       plugin_module.define_singleton_method :included do |example_group|
 
-        plugin = plugin_module.send :extend_object, Object.new
-        plugin.define_singleton_method(:plugin_module) { plugin_module }
-        plugin.define_singleton_method(:example_group) { example_group }
+        plugin = plugin_module::Plugin.new(example_group, plugin_module)
 
         settings = plugin_module.settings
         helpers = settings.helpers
         listeners = settings.listeners
         hooks =  settings.hooks
 
-        # make plugin settings accessible in specs
-        example_group.let(settings.settings_accessor) { plugin.storage }
+        example_group.metadata[:plugins] ||= {}
+        example_group.metadata[:plugins][plugin_module] = plugin
 
         # -- define helper methods
         helpers.values.each do |helper|
@@ -61,17 +69,15 @@ module RSpec::Plugins
     end
 
     def method_missing(symbol, *args, &block)
-      listener = plugin_module.settings.listeners[symbol]
-      return listener.block.call(self, *args) if ! listener.nil?
+      if ! methods.index(:plugin_module).nil?
+        listener = plugin_module.settings.listeners[symbol]
+        return listener.block.call(self, *args) if ! listener.nil?
+      end
       raise NoMethodError, "non existing method called: #{symbol}"
     end
 
     def respond_to_missing?(symbol, include_private)
       ! plugin_module.settings.listeners[symbol].nil?
-    end
-
-    def storage
-      @storage ||= OpenStruct.new
     end
 
     class Hook
@@ -93,23 +99,18 @@ module RSpec::Plugins
 
     module ClassMethods
       def settings
-        settings_key = self.to_s.to_sym
-        Thread.current[settings_key] ||= OpenStruct.new(
+        id = self.to_s.to_sym
+        Thread.current[id] ||= OpenStruct.new(
             {:listeners    => {},
              :helpers      => {},
              :hooks        => [],
-             :settings_accessor => "#{settings_key.downcase}_settings".to_sym
+             :id          => id
             })
-      end
-
-      def settings_accessor(symbol)
-        settings.settings_accessor = symbol
       end
 
       def hook(position, target, &block)
         settings.hooks << Hook.new(position, target, &block)
       end
-
 
       # Register a notification callback.
       # {RSpec Reporter Notifications}[rdoc-href:RSpec::Core::Reporter::NOTIFICATIONS]
